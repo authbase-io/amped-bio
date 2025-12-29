ce300530fd39bf47668c0c8e932149772ea04713
Anurag Chemban <anurag.chemban@ideacrestsolutions.com>
2025-12-29 13:48:03 +0530

    updated types

--- apps/client/src/types/rns/registration.ts
+++ apps/client/src/types/rns/registration.ts
@@ -1,9 +1,17 @@
 export interface RegistrationData {
+  revoNames: {
+    name: string;
+    owner: `0x${string}`;
+    expiryDateWithGrace: string;
+    resolver: {
+      address: `0x${string}`;
+    };
+  };
   registration: {
     registrationDate: string;
     expiryDate: string;
   };
   nameRegistereds: {
-    transactionID: string;
+    transactionID: `0x${string}`;
   }[];
 }
